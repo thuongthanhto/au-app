6c2f2a8444d7b556d5e513968a3de2cb77b885be
Kane <dinhtientrinh58@gmail.com>
2019-12-19 03:05:30 +0700

    custome tabbar bottom icon

---
+++ src/assets/images/index.js
@@ -0,0 +1,12 @@
+export const Images = {
+  burn: require('../icons/tabbar/burn.png'),
+  burn_on: require('../icons/tabbar/burn_on.png'),
+  converter: require('../icons/tabbar/converter.png'),
+  converter_on: require('../icons/tabbar/converter_on.png'),
+  more: require('../icons/tabbar/more.png'),
+  more_on: require('../icons/tabbar/more_on.png'),
+  search: require('../icons/tabbar/search.png'),
+  search_on: require('../icons/tabbar/search_on.png'),
+  ideal_figure: require('../icons/tabbar/ideal-figure.png'),
+  ideal_figure_on: require('../icons/tabbar/ideal-figure_on.png'),
+};
--- src/navigator/TabNavigator.js
+++ src/navigator/TabNavigator.js
@@ -1,4 +1,6 @@
 import { createBottomTabNavigator } from 'react-navigation';
+import { Image } from 'react-native';
+import React from 'react';
 
 import MoreStack from './MoreStack';
 import BurnkJStack from './BurnkJStack';
@@ -6,21 +8,60 @@ import SearchStack from './SearchStack';
 import { NavigationRoutes } from './Routes';
 import ConverterStack from './ConverterStack';
 import IdealFigureStack from './IdealFigureStack';
+import { Images } from '../assets/images';
 
-export default createBottomTabNavigator({
-  [NavigationRoutes.Search]: {
-    screen: SearchStack
-  },
-  [NavigationRoutes.IdealFigure]: {
-    screen: IdealFigureStack
-  },
-  [NavigationRoutes.Converter]: {
-    screen: ConverterStack
-  },
-  [NavigationRoutes.BurnkJ]: {
-    screen: BurnkJStack
-  },
-  [NavigationRoutes.More]: {
-    screen: MoreStack
+const getTabBarIcon = (navigation, focused) => {
+  const { routeName } = navigation.state;
+  let iconName = '';
+
+  if (routeName === NavigationRoutes.BurnkJ) {
+    iconName = !focused ? 'burn' : `burn_on`;
+  } else if (routeName === NavigationRoutes.IdealFigure) {
+    iconName = !focused ? 'ideal_figure' : `ideal_figure_on`;
+  } else {
+    iconName = !focused ? routeName.toLowerCase() : `${routeName.toLowerCase()}_on`;
   }
-});
+  
+  return (
+    <Image
+      source={Images[iconName]}
+      style={{
+        width: 25,
+        height: 25,
+        paddingVertical: 10
+      }}
+    />
+  );
+};
+
+export default createBottomTabNavigator(
+  {
+    [NavigationRoutes.Search]: {
+      screen: SearchStack
+    },
+    [NavigationRoutes.IdealFigure]: {
+      screen: IdealFigureStack
+    },
+    [NavigationRoutes.Converter]: {
+      screen: ConverterStack
+    },
+    [NavigationRoutes.BurnkJ]: {
+      screen: BurnkJStack
+    },
+    [NavigationRoutes.More]: {
+      screen: MoreStack
+    }
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused }) => getTabBarIcon(navigation, focused)
+    }),
+    tabBarOptions: {
+      inactiveTintColor: 'white',
+      activeTintColor: 'white',
+      allowFontScaling: true,
+      activeBackgroundColor: 'black',
+      inactiveBackgroundColor: 'black',
+    }
+  }
+);
