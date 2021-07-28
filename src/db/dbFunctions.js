77dfcc325b8b918d3d83864e172994896586b11a
Thuong To <thuongthanhto@gmail.com>
2021-07-28 15:11:22 +0700

    Fix search term

--- src/db/dbFunctions.js
+++ src/db/dbFunctions.js
@@ -24,7 +24,7 @@ export function searchProduct(
       });
     } else {
       arraysClone = arraysClone.filter(item => {
-        return item.Name.includes(searchTerm);
+        return item.Name.toLowerCase().includes(searchTerm.toLowerCase());
       });
     }
   }
